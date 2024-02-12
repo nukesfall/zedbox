60331bbe55e7c206c2126f5f9ee8d0f3c5b3395f
Logan <156613580+nukesfall@users.noreply.github.com>
2024-02-11 18:43:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Display "Access Denied" message
+    alert('Access Denied');
+});
